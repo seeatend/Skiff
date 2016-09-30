bbf2b66bf64d608ea1dcaf37d51c3cd1bdbb52df
seeatend <readydone19@gmail.com>
2016-09-30 09:01:22 -0400

    docker cleanup

--- mock/dbinit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-postgres
-./root/wait-for-it.sh localhost:5432 -t 60
-createdb -h 127.0.0.1 -p 5432 -U postgres -w -e sandbar_dev
-pg_restore -h localhost -d postgres -U postgres ~/dump.pg
