9975be5c5acd260439c6be7144e4ae1c315006ae
seeatend <readydone19@gmail.com>
2016-10-26 08:24:10 -0400

    inital contextual pane impl

--- src/js/views/App.tsx
+++ src/js/views/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Navbar } from './containers/navigation/Navbar';
+import { ContextPane } from './containers/pane/ContextPane';
  
 export class App extends React.Component<void, void> {
     public render() {
         return (
             <div>
-                <Navbar />                    
+                <Navbar />
+                <ContextPane />
                 <div id="content">
                     { this.props.children }
                 </div> 
