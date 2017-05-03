cb18e7d63e029189055f4591152a0213c1e18a97
seeatend <readydone19@gmail.com>
2017-05-03 19:49:11 +0200

    cleanup, commit test

--- server.js
+++ server.js
@@ -6,32 +6,7 @@ const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const config = require('./webpack.config')
 
-// serve static assets normally
-// if(process.env.NODE_ENV === "production") {
-
 app.use(express.static('dist/'));
-
-// handle every other route with index.html, which will contain
-// a script tag to your application's JavaScript file(s).
-// app.get('*', function (request, response) {
-//     console.log(path.resolve(__dirname, 'dist', 'index.html'));
-//     response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-// });
 
 app.listen(port);
 console.log("server started on port " + port);
-
-/*
- } else {
-
- const server = new WebpackDevServer(webpack(config), {
- publicPath: config.output.publicPath,
- hot: true,
- historyApiFallback: true,
- port: port,
- host: '0.0.0.0'
- });
-
- server.listen(port);
- }
- */
